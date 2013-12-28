3d37f77db0353463098d08389a635438653e5892
Bob Yao <byao@salesforce.com>
2013-12-27 16:38:50 -0800

    clean up and add production build

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+
     // Project Configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -55,7 +56,7 @@ module.exports = function(grunt) {
             }
         },
         concurrent: {
-            tasks: ['nodemon', 'watch'],
+            tasks: ['nodemon:dev', 'watch'],
             options: {
                 logConcurrentOutput: true
             }
@@ -75,24 +76,76 @@ module.exports = function(grunt) {
             unit: {
                 configFile: 'test/karma/karma.conf.js'
             }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            dist: {
+                files: {
+                    'public/prod/osc.js' : [
+                        'public/lib/angular/angular.js',
+                        'public/lib/angular-cookies/angular-cookies.js',
+                        'public/lib/angular-resource/angular-resource.js',
+                        'public/lib/angular-route/angular-route.js',
+                        'public/lib/angular-bootstrap/ui-bootstrap.js',
+                        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+                        'public/js/app.js',
+                        'public/js/config.js',
+                        'public/js/directives.js',
+                        'public/js/services/*.js',
+                        'public/js/constrollers/*.js',
+                        'public/js/init.js',
+                    ]
+                }
+            }
+        },
+        cssmin:  {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            dist: {
+                files: {
+                    'public/prod/osc.css' : [
+                        'public/lib/bootstrap/docs/assets/css/bootstrap.css',
+                        'public/lib/bootstrap/docs/assets/css/bootstrap-responsive.css',
+                        'public/css/common.css',
+                        'public/css/views/**/*.css'
+                    ]
+                }
+            }
+        },
+
+        forever: {
+            options: {
+                index: 'server.js',
+                logDir: 'logs'
+            }
         }
     });
 
-    //Load NPM tasks 
+    // Load NPM tasks 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-forever');
 
-    //Making grunt default to force in order not to break the project.
+
+    // Making grunt default to force in order not to break the project.
     grunt.option('force', true);
 
-    //Default task(s).
+    // Development
     grunt.registerTask('default', ['jshint', 'concurrent']);
 
-    //Test task.
+    // Test
     grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+
+    // Production
+    grunt.registerTask('production', ['jshint', 'cssmin', 'uglify', 'forever:start']);
 };
